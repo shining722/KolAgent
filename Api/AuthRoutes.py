9be36e6108d7de76a9d0464f4b9d367d0cb78a52
wormtown <445665429@qq.com>
2025-01-28 22:07:25 +0800

    twitter auth

--- Api/AuthRoutes.py
+++ Api/AuthRoutes.py
@@ -1,8 +1,20 @@
 #AuthRoutes.py
 
 from flask import Blueprint
+import tweepy
 
 auth_bp = Blueprint('auth', __name__)
+
+@auth_bp.route('/twitter/auth')
+def twitter_auth():
+    bearer_token = '你的Bearer Token'
+    client = tweepy.Client(bearer_token=bearer_token)
+    # 使用Client对象进行操作，例如搜索推文
+    tweets = client.search_recent_tweets(query='twitter', max_results=10)
+    for tweet in tweets.data:
+        print(tweet.text)
+
+
 
 @auth_bp.route('/login')
 def login():
